d11edb06215afeb7e44972985dbba5a59bdd9017
Abdulmalik Ariyo <malikariyo@hotmail.com>
2024-12-22 10:19:58 -0700

    Initial Commit

---
+++ Swipe or Keep/Swipe_or_KeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_or_KeepApp.swift
+//  Swipe or Keep
+//
+//  Created by Gbolade Ariyo on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_or_KeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swipe or KeepTests/Swipe_or_KeepTests.swift
@@ -0,0 +1,16 @@
+//
+//  Swipe_or_KeepTests.swift
+//  Swipe or KeepTests
+//
+//  Created by Gbolade Ariyo on 12/22/24.
+//
+
+import Testing
+
+struct Swipe_or_KeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
